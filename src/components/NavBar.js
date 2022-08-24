47ce7031ff4bc8badadf1d2e8aaafe9f36e3decf
Javier Salafica <jsalafica@gmail.com>
2022-08-23 21:27:25 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return(
+        <div className="navBar">
+            <div>Algo</div>
+            <div>Algo 2</div>
+            <div>Algo 3</div>
+        </div>
+    )
+}
+
+export default NavBar;
