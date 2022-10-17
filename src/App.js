9cb7eb67e8eb3b038440abb86b75624884f8b380
Javier Salafica <jsalafica@gmail.com>
2022-10-17 17:57:02 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.js";
-import Titulo from "./components/Titulo/Titulo";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import CartProvider  from "./context/CartProvider";
 import Cart from "./components/Cart/Cart";
